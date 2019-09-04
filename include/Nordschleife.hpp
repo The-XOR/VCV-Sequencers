cfd893691d64faa2d0cfbbbd7cd5acbc60044f7f
The XOR <1h3x0r@gmail.com>
2019-09-04 23:10:56 +0200

    selezione step

--- include/Nordschleife.hpp
+++ include/Nordschleife.hpp
@@ -8,10 +8,51 @@ extern Plugin *pluginInstance;
 struct Nordschleife;
 struct NordschleifeWidget : SequencerWidget
 {
+	enum MENUACTIONS
+	{
+		RANDOMIZE_PITCH = 0x01,
+		RANDOMIZE_LAQUALUNQUE = 0x10,
+		QUANTIZE_PITCH
+	};
+
+	struct RandomizeSubItemItem : MenuItem
+	{
+		RandomizeSubItemItem(Module *m, const char *title, int action);
+
+		int randomizeDest;
+		Nordschleife *md;
+		void onAction(const event::Action &e) override;
+	};
+
+	struct RandomizeItem : ui::MenuItem
+	{
+	public:
+		RandomizeItem(Module *m)
+		{
+			md = m;
+			text = "Force the hand of chance";
+			rightText = RIGHT_ARROW;
+		};
+		Menu *createChildMenu() override
+		{
+			Menu *sub_menu = new Menu;
+			sub_menu->addChild(new RandomizeSubItemItem(md, "Ov Pitch", RANDOMIZE_PITCH));
+			sub_menu->addChild(new RandomizeSubItemItem(md, "Ov Power", RANDOMIZE_LAQUALUNQUE));
+			return sub_menu;
+		}
+	private:
+		Module *md;
+	};
+
+	void onHoverKey(const event::HoverKey &e) override;
+
 	NordschleifeWidget(Nordschleife *module);
 	void createStep(Nordschleife *module, int index);
 	void createCar(Nordschleife *module, int index);
 	void createDataEntry(Nordschleife *module);
+	void onMenu(int action);
+	Menu *addContextMenu(Menu *menu) override;
+
 };
 
 struct Nordschleife : Module
@@ -21,7 +62,7 @@ struct Nordschleife : Module
 		VOLTAGE_1,
 		STEPSELECT_1 = VOLTAGE_1 + NORDSTEPS,
 		CAR_SELECT = STEPSELECT_1 + NORDSTEPS,
-		DATAENTRY_OK = CAR_SELECT + NORDCARS,
+		DATAENTRY_MODE = CAR_SELECT + NORDCARS,
 		DATAENTRY_UP,
 		DATAENTRY_DOWN,
 		DATAENTRY_LEFT,
@@ -58,6 +99,8 @@ struct Nordschleife : Module
 
 	Nordschleife() : Module()
 	{
+		pWidget = NULL;
+		theRandomizer = 0;
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 	}
 	void process(const ProcessArgs &args) override;
@@ -65,23 +108,77 @@ struct Nordschleife : Module
 	cvStrip cvs;
 	int selectedCar = 0;
 	int selectedStep = 0;
+	int theRandomizer;
 
-	private:
-	void car_select();
-	void step_select();
+	void dataFromJson(json_t *root) override
+	{
+		Module::dataFromJson(root);
+		json_t *rndJson = json_object_get(root, "theRandomizer");
+		if(rndJson)
+			theRandomizer = json_integer_value(rndJson);
+		on_loaded();
+	}
+	json_t *dataToJson() override
+	{
+		INFO("to json");
+		json_t *rootJ = json_object();
+		json_t *rndJson = json_integer(theRandomizer);
+		json_object_set_new(rootJ, "theRandomizer", rndJson);
+		return rootJ;
+	}
+	void QuantizePitch();
+	void setWidget(NordschleifeWidget *pwdg) { pWidget = pwdg; }
 	inline void setCar(int n)
 	{
 		selectedCar = n;
 		for(int k = 0; k < NORDCARS; k++)
 			params[CAR_SELECT + k].setValue(k == selectedCar);
 	}
+	inline void toggleDataEntryMode()
+	{
+		params[DATAENTRY_MODE].setValue(params[DATAENTRY_MODE].getValue() == 0);
+	}
+	inline void setKey(int code) { key = code; }
+
+	private:
+	void car_select();
+	void step_select();
+	
 	inline void setStep(int n)
 	{
 		selectedStep = n;
 		for(int k = 0; k < NORDSTEPS; k++)
 			params[STEPSELECT_1 + k].setValue(k == selectedStep);
 	}
-	private:
+private:
+		void randrandrand();
+		void randrandrand(int action);
+		void on_loaded();
+		void load();
+		void data_entry();
+		NordschleifeWidget *pWidget;
+		void reset() 
+		{
+			lazyCheck = 0;
+		}
+
+		inline bool consumeKey(int code)
+		{
+			if(key == code)
+			{
+				key = 0;
+				return true;
+			}
+			return false;
+		}
+
 	dsp::SchmittTrigger carSelectTrigger[NORDCARS];
 	dsp::SchmittTrigger stepSelectTrigger[NORDSTEPS];
+	dsp::SchmittTrigger rndTrigger;
+	dsp::SchmittTrigger leftKey;
+	dsp::SchmittTrigger rightKey;
+	dsp::SchmittTrigger upKey;
+	dsp::SchmittTrigger downKey;
+	int lazyCheck = 0;
+	int key = 0;
 };
--- src/Nordschleife.cpp
+++ src/Nordschleife.cpp
@@ -3,18 +3,70 @@
 
 void Nordschleife::process(const ProcessArgs &args)
 {
+	if(pWidget != NULL && rndTrigger.process(inputs[RANDOMIZONE].value))
+		randrandrand();
+
 	car_select();
-	step_select();
+	if(lazyCheck++ % 20 == 0)
+		step_select();
+	data_entry();
 }
 
 void Nordschleife::car_select()
 {
 	for(int k = 0; k < NORDCARS; k++)
 	{
-		if(carSelectTrigger[k].process(params[CAR_SELECT+k].getValue()))
+		if(carSelectTrigger[k].process(params[CAR_SELECT + k].getValue()))
 		{
 			setCar(k);
 			break;
+		}
+	}
+}
+
+void Nordschleife::data_entry()
+{
+	if(consumeKey(GLFW_KEY_ENTER))
+	{
+		toggleDataEntryMode();
+		return;
+	}
+
+	int move = 0;
+	if(consumeKey(GLFW_KEY_KP_MULTIPLY) || rightKey.process(params[DATAENTRY_RIGHT].getValue()))
+		move = GLFW_KEY_KP_MULTIPLY;
+	else if(consumeKey(GLFW_KEY_KP_DIVIDE) || leftKey.process(params[DATAENTRY_LEFT].getValue()))
+		move = GLFW_KEY_KP_DIVIDE;
+	else if(consumeKey(GLFW_KEY_KP_SUBTRACT) || upKey.process(params[DATAENTRY_UP].getValue()))
+		move = GLFW_KEY_KP_SUBTRACT;
+	else if(consumeKey(GLFW_KEY_KP_ADD) || downKey.process(params[DATAENTRY_DOWN].getValue()))
+		move = GLFW_KEY_KP_ADD;
+
+	if(params[DATAENTRY_MODE].getValue() == 0)  //move by step?
+	{
+		int r = selectedStep / 8;
+		int c = selectedStep % 8;
+		int curStep = selectedStep;
+		switch(move)
+		{
+			case GLFW_KEY_KP_DIVIDE:
+				if(--curStep < r * 8) curStep = r * 8 + 7;
+				setStep(curStep);
+				break;
+			case GLFW_KEY_KP_MULTIPLY:
+				if(++curStep > 7+r * 8) curStep = r * 8;
+				setStep(curStep);
+				break;
+			case GLFW_KEY_KP_SUBTRACT:
+				curStep -= 8;
+				if(curStep< 0) curStep = 56+c;
+				setStep(curStep);
+				break;
+			case GLFW_KEY_KP_ADD:
+				curStep += 8;
+				if(curStep > 63) curStep = c;
+				setStep(curStep);
+				break;
 		}
 	}
 }
@@ -23,12 +75,50 @@ void Nordschleife::step_select()
 {
 	for(int k = 0; k < NORDSTEPS; k++)
 	{
-		if(stepSelectTrigger[k].process(params[STEPSELECT_1+k].getValue()))
+		if(stepSelectTrigger[k].process(params[STEPSELECT_1 + k].getValue()))
 		{
 			setStep(k);
 			break;
 		}
 	}
+}
+
+void Nordschleife::on_loaded()
+{
+	load();
+	cvs.Init(pWidget);
+}
+
+void Nordschleife::load()
+{
+	reset();
+}
+
+void Nordschleife::randrandrand()
+{
+	if(theRandomizer & NordschleifeWidget::RANDOMIZE_PITCH)
+		randrandrand(0);
+
+	if(theRandomizer & NordschleifeWidget::RANDOMIZE_LAQUALUNQUE)
+	{
+		randrandrand(int(random::uniform() * 1));
+	}
+}
+
+void Nordschleife::randrandrand(int action)
+{
+	switch(action)
+	{
+		case 0:
+			pWidget->std_randomize(Nordschleife::VOLTAGE_1, Nordschleife::VOLTAGE_1 + NORDSTEPS);
+			break;
+	}
+}
+
+void Nordschleife::QuantizePitch()
+{
+	for(int k = 0; k < NORDSTEPS; k++)
+		params[VOLTAGE_1 + k].value = pWidget->quantizePitch(VOLTAGE_1 + k, params[VOLTAGE_1 + k].value, cvs);
 }
 
 NordschleifeWidget::NordschleifeWidget(Nordschleife *module)
@@ -40,12 +130,13 @@ NordschleifeWidget::NordschleifeWidget(Nordschleife *module)
 		createCar(module, k);
 
 	createDataEntry(module);
-	
+
 	if(module != NULL)
 	{
+		module->setWidget(this);
 		nordDisplay *display = createWidget<nordDisplay>(Vec(mm2px(185.942), yncscape(82.4f, 41.f)));
 		display->setModule(module);
-		addChild(display);			
+		addChild(display);
 
 		module->cvs.Create(this, 236.228f, 15.367f, Nordschleife::NUM_INPUTS - cvStrip::CVSTRIP_INPUTS, Nordschleife::NUM_PARAMS - cvStrip::CVSTRIP_PARAMS, NORDSTEPS);
 	}
@@ -55,14 +146,14 @@ NordschleifeWidget::NordschleifeWidget(Nordschleife *module)
 
 void NordschleifeWidget::createDataEntry(Nordschleife *module)
 {
-	float x =234.672f;
-	float y =113.052f;
+	float x = 234.672f;
+	float y = 113.052f;
 
-	addParam(createParam<CKD6B>(Vec(mm2px(x), yncscape(y, 6.f)), module, Nordschleife::DATAENTRY_OK));
-	addParam(createParam<UPSWITCH>(Vec(mm2px(x+0.686f), yncscape(y+6.704f, 4.115f)), module, Nordschleife::DATAENTRY_UP));
-	addParam(createParam<DNSWITCH>(Vec(mm2px(x+0.686f), yncscape(y-4.819f, 4.115f)), module, Nordschleife::DATAENTRY_DOWN));
-	addParam(createParam<LEFTSWITCH>(Vec(mm2px(x-4.819f), yncscape(y+0.686f, 4.627f)), module, Nordschleife::DATAENTRY_LEFT));
-	addParam(createParam<RIGHTSWITCH>(Vec(mm2px(x+6.704f), yncscape(y+0.686f, 4.627f)), module, Nordschleife::DATAENTRY_RIGHT));
+	addParam(createParam<CKD6B>(Vec(mm2px(x), yncscape(y, 6.f)), module, Nordschleife::DATAENTRY_MODE));
+	addParam(createParam<UPSWITCH>(Vec(mm2px(x + 0.686f), yncscape(y + 6.704f, 4.115f)), module, Nordschleife::DATAENTRY_UP));
+	addParam(createParam<DNSWITCH>(Vec(mm2px(x + 0.686f), yncscape(y - 4.819f, 4.115f)), module, Nordschleife::DATAENTRY_DOWN));
+	addParam(createParam<LEFTSWITCH>(Vec(mm2px(x - 4.819f), yncscape(y + 0.686f, 4.627f)), module, Nordschleife::DATAENTRY_LEFT));
+	addParam(createParam<RIGHTSWITCH>(Vec(mm2px(x + 6.704f), yncscape(y + 0.686f, 4.627f)), module, Nordschleife::DATAENTRY_RIGHT));
 }
 
 void NordschleifeWidget::createCar(Nordschleife *module, int index)
@@ -73,9 +164,9 @@ void NordschleifeWidget::createCar(Nordschleife *module, int index)
 	addInput(createInput<PJ301YPort>(Vec(mm2px(x), yncscape(52.759f, 8.255f)), module, Nordschleife::CAR_RESET + index));
 	addInput(createInput<PJ301RPort>(Vec(mm2px(x), yncscape(41.540f, 8.255f)), module, Nordschleife::CAR_CLOCK + index));
 	addOutput(createOutput<PJ301GPort>(Vec(mm2px(x), yncscape(30.322f, 8.255f)), module, Nordschleife::CAR_CV + index));
-	addOutput(createOutput<PJ301WPort>(Vec(mm2px(x), yncscape(19.103f, 8.255f)), module, Nordschleife::CAR_GATE+index));
-	addOutput(createOutput<PJ301BLUPort>(Vec(mm2px(x), yncscape(63.978f, 8.255f)), module, Nordschleife::CAR_LAP+index));
-	addParam(createParam<CKD6B>(Vec(mm2px(x + 0.423f), yncscape(1.225f, 6.f)), module, Nordschleife::CAR_SELECT+ index));
+	addOutput(createOutput<PJ301WPort>(Vec(mm2px(x), yncscape(19.103f, 8.255f)), module, Nordschleife::CAR_GATE + index));
+	addOutput(createOutput<PJ301BLUPort>(Vec(mm2px(x), yncscape(63.978f, 8.255f)), module, Nordschleife::CAR_LAP + index));
+	addParam(createParam<CKD6B>(Vec(mm2px(x + 0.423f), yncscape(1.225f, 6.f)), module, Nordschleife::CAR_SELECT + index));
 }
 
 void NordschleifeWidget::createStep(Nordschleife *module, int index)
@@ -91,14 +182,77 @@ void NordschleifeWidget::createStep(Nordschleife *module, int index)
 	if(c == 0 || c == 4)
 		addParam(createParam<Davies1900hFixBlackKnob>(Vec(mm2px(x), yncscape(y, 9.525f)), module, Nordschleife::VOLTAGE_1 + index));
 	else
-		addParam(createParam<Davies1900hFixRedKnob>(Vec(mm2px(x), yncscape(y, 9.525f)), module, Nordschleife::VOLTAGE_1+index));
+		addParam(createParam<Davies1900hFixRedKnob>(Vec(mm2px(x), yncscape(y, 9.525f)), module, Nordschleife::VOLTAGE_1 + index));
 	addParam(createParam<StepSelector>(Vec(mm2px(x), yncscape(y - 2.532f, 1.879f)), module, Nordschleife::STEPSELECT_1 + index));
 
-	addOutput(createOutput<portSmall>(Vec(mm2px(x+13.222), yncscape(y+3.365, 5.885f)), module, Nordschleife::OUT_A + index));
-	addOutput(createOutput<portSmall>(Vec(mm2px(x+13.222), yncscape(y-2.532, 5.885f)), module, Nordschleife::OUT_B + index));
+	addOutput(createOutput<portSmall>(Vec(mm2px(x + 13.222), yncscape(y + 3.365, 5.885f)), module, Nordschleife::OUT_A + index));
+	addOutput(createOutput<portSmall>(Vec(mm2px(x + 13.222), yncscape(y - 2.532, 5.885f)), module, Nordschleife::OUT_B + index));
 
 	addChild(createLight<SmallLight<GreenLight>>(Vec(mm2px(x + 10.339f), yncscape(y + 7.35f, 2.176f)), module, Nordschleife::LOTUS_LED + index));
 	addChild(createLight<SmallLight<WhiteLight>>(Vec(mm2px(x + 10.339f), yncscape(y + 4.027f, 2.176f)), module, Nordschleife::BRABHAM_LED + index));
-	addChild(createLight<SmallLight<RedLight>>  (Vec(mm2px(x + 10.339f), yncscape(y + 0.792f, 2.176f)), module, Nordschleife::FERRARI_LED + index));
-	addChild(createLight<SmallLight<PurpleLight>>(Vec(mm2px(x + 10.339f), yncscape(y - 2.532f, 2.176f)), module, Nordschleife::HESKETH_LED +index));
+	addChild(createLight<SmallLight<RedLight>>(Vec(mm2px(x + 10.339f), yncscape(y + 0.792f, 2.176f)), module, Nordschleife::FERRARI_LED + index));
+	addChild(createLight<SmallLight<PurpleLight>>(Vec(mm2px(x + 10.339f), yncscape(y - 2.532f, 2.176f)), module, Nordschleife::HESKETH_LED + index));
+}
+
+Menu *NordschleifeWidget::addContextMenu(Menu *menu)
+{
+	menu->addChild(new RandomizeItem(module));
+
+	menu->addChild(new SeqMenuItem<NordschleifeWidget>("Randomize Pitch", this, RANDOMIZE_PITCH));
+	menu->addChild(new SeqMenuItem<NordschleifeWidget>("Pitch Quantization", this, QUANTIZE_PITCH));
+	return menu;
+}
+
+void NordschleifeWidget::onMenu(int action)
+{
+	switch(action)
+	{
+		case RANDOMIZE_PITCH: std_randomize(Nordschleife::VOLTAGE_1, Nordschleife::VOLTAGE_1 + NORDSTEPS); break;
+		case QUANTIZE_PITCH: ((Nordschleife *)module)->QuantizePitch(); break;
+	}
+}
+
+void NordschleifeWidget::RandomizeSubItemItem::onAction(const event::Action &e)
+{
+	md->theRandomizer ^= randomizeDest;
+}
+
+NordschleifeWidget::RandomizeSubItemItem::RandomizeSubItemItem(Module *m, const char *title, int action)
+{
+	md = (Nordschleife *)m;
+	text = title;
+	randomizeDest = action;
+	rightText = CHECKMARK((md->theRandomizer & randomizeDest) != 0);
+}
+
+void NordschleifeWidget::onHoverKey(const event::HoverKey &e)
+{
+	SequencerWidget::onHoverKey(e);
+	if(module != NULL && !e.isConsumed() && e.action == GLFW_PRESS)
+	{
+		switch(e.key)
+		{
+			case GLFW_KEY_KP_1:
+			case GLFW_KEY_KP_2:
+			case GLFW_KEY_KP_3:
+			case GLFW_KEY_KP_4:
+				e.consume(this);
+				((Nordschleife *)module)->setCar(e.key - GLFW_KEY_KP_1);
+				break;
+
+			case GLFW_KEY_KP_ENTER:
+			case GLFW_KEY_ENTER:
+				e.consume(this);
+				((Nordschleife *)module)->setKey(GLFW_KEY_ENTER);
+				break;
+
+			case GLFW_KEY_KP_DIVIDE:
+			case GLFW_KEY_KP_MULTIPLY:
+			case GLFW_KEY_KP_SUBTRACT:
+			case GLFW_KEY_KP_ADD:
+				e.consume(this);
+				((Nordschleife *)module)->setKey(e.key);
+				break;
+		}
+	}
 }
