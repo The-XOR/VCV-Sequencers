5d07aadd8e6db60109ca410ebe114d420533345e
The-XOR <1h3x0r@gmail.com>
2020-06-23 08:16:56 +0200

    c2v

--- src/c2v.cpp
+++ src/c2v.cpp
@@ -2,11 +2,14 @@
 
 void c2v::process(const ProcessArgs &args)
 {
-	for(int k = 0; k < C2VCNV; k++)
+	float v = 0;
+	for (int k = 0; k < C2VCNV; k++)
 	{
-		if(outputs[OUT_1+k].isConnected())
-			outputs[OUT_1+k].setVoltage(inputs[IN_1+k].getVoltage() > 0 ? params[CV_1+k].value : 0.0);
+		if(inputs[IN_1 + k].getNormalVoltage(0.f) > 0)
+			v += params[CV_1+k].value;
 	}
+
+	outputs[OUT_1].setVoltage(v);
 }
 
 
@@ -19,7 +22,6 @@ c2vWidget::c2vWidget(c2v *module)
 	{
 		addInput(createInput<portSmall>(Vec(mm2px(1.492), yncscape(y-k*yinc, 5.885)), module, c2v::IN_1+k));
 		addParam(createParam<Davies1900hFixWhiteKnobSmall>(Vec(mm2px(8.704), yncscape(115.446-k*yinc, 8)), module, c2v::CV_1+k));
-		addOutput(createOutput<portGSmall>(Vec(mm2px(18.023), yncscape(y-k*yinc, 5.885)), module, c2v::OUT_1+k));
 	}
+	addOutput(createOutput<portGSmall>(Vec(mm2px(18.023), yncscape(61.214, 5.885)), module, c2v::OUT_1));
 }
-
